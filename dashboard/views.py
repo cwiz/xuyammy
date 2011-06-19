ae463999f1de9062c137cef0d3f60121439e16a8
Evgeny Kuryshev <evgeny@ostrovok.ru>
2011-06-19 20:07:08 +0400

    add_task and data views updated

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,11 +6,12 @@ from django.core import serializers
 from django.utils import simplejson
 from django.forms.models import model_to_dict
 from django.core.serializers.json import DjangoJSONEncoder
-from django.contrib.admin.models import LogEntry, ADDITION
+from django.contrib.admin.models import LogEntry, ADDITION, CHANGE, DELETION
 from django.contrib.contenttypes.models import ContentType
 from django.utils.encoding import force_unicode
 import json
 import datetime
+import time
 
 from django.core.exceptions import ObjectDoesNotExist
  
@@ -19,61 +20,70 @@ def index(request):
 	return render(request, 'index.html')
 
 def data(request):
-    return render(request, 'data.html', {
-        'tasks' :   serialize_model(Task),
-        'stories' : serialize_model(Story),
-        'tags' :    serialize_model(Tag),
-        'timestamp' : datetime.datetime.now(),
-    })
+    if 'timestamp' in request.GET:
+        try:
+            # trying to get all objects changed/added since timestamp
+            entries = LogEntry.objects.filter(
+                action_time__gt = datetime.datetime.fromtimestamp(int(request.GET['timestamp'])),
+            )
+            result = {'tasks' : {}, 'stories' : {}, 'tags' : {}, 'timestamp' : time.time()}
+            for e in entries:
+                try:
+                    obj = e.get_edited_object()
+                except ObjectDoesNotExist:
+                    continue
+                if isinstance(obj, Task):
+                    result['tasks'][obj.id] = json.dumps(model_to_dict(obj), cls=DjangoJSONEncoder, indent=4)
+                elif isinstance(obj, Story):
+                    result['stories'][obj.id] = json.dumps(model_to_dict(obj), cls=DjangoJSONEncoder, indent=4)
+                elif isinstance(obj, Tag):
+                    result['tags'][obj.id] = json.dumps(model_to_dict(obj), cls=DjangoJSONEncoder, indent=4)
+
+            return render(request, 'data.html', result)
+
+        except KeyError:
+            return HttpResponse('{"status" : "Fail", "reason" : "Missing timestamp GET parameter"}')
+    else:
+        return render(request, 'data.html', {
+            'tasks' :   serialize_model(Task),
+            'stories' : serialize_model(Story),
+            'tags' :    serialize_model(Tag),
+            'timestamp' : time.time(),
+        })
 
 def add_task(request):
+    if 'id' in request.GET:
+        t = Task.objects.get(id=request.GET.id)
+        t.save()
+        flag = CHANGE
     try:
     # get related story 
         s = Story.objects.get(id=request.GET['story_id'])
         # create task object 
         t = Task(description=request.GET['description'], story=s)
         t.save()
+        flag = ADDITION
 
-        # now log addition
-        LogEntry.objects.log_action(
-            user_id         = request.user.pk, 
-            content_type_id = ContentType.objects.get_for_model(t).pk,
-            object_id       = t.pk,
-            object_repr     = force_unicode(t), 
-            action_flag     = ADDITION
-        )
-        return HttpResponse('{"status" : "Success", "task_id" : t.id }')
     except KeyError:
         return HttpResponse('{"status" : "Fail", "reason" : "Missing some of the required parameters: description, strory_id"}')
     except DoesNotExist:
         return HttpResponse('{"status" : "Fail", "reason" : "It seems story_id is pointing on the non-existent story"}')
-        
-    return render(request, 'index.html')
-    
-def update(request):
-    try:
-        # trying to get all objects changed/added since timestamp
-        entries = LogEntry.objects.filter(
-            action_time__gt = datetime.datetime.fromtimestamp(int(request.GET['timestamp'])),
-        )
-        result = {'tasks' : {}, 'stories' : {}, 'tags' : {}}
-        for e in entries:
-            try:
-                obj = e.get_edited_object()
-            except ObjectDoesNotExist:
-                continue
-            if isinstance(obj, Task):
-                result['tasks'][obj.id] = json.dumps(model_to_dict(obj), cls=DjangoJSONEncoder, indent=4)
-            elif isinstance(obj, Story):
-                result['stories'][obj.id] = json.dumps(model_to_dict(obj), cls=DjangoJSONEncoder, indent=4)
-            elif isinstance(obj, Tag):
-                result['tags'][obj.id] = json.dumps(model_to_dict(obj), cls=DjangoJSONEncoder, indent=4)
 
-        return render(request, 'data.html', result)
+    
         
-    except KeyError:
-        return HttpResponse('{"status" : "Fail", "reason" : "Missing timestamp GET parameter"}')
-        
+    # now log addition
+    LogEntry.objects.log_action(
+        user_id         = request.user.pk, 
+        content_type_id = ContentType.objects.get_for_model(t).pk,
+        object_id       = t.pk,
+        object_repr     = force_unicode(t), 
+        action_flag     = flag
+    )
+    
+    task_json = json.dumps(model_to_dict(t), cls=DjangoJSONEncoder, indent=4)
+    
+    return HttpResponse('{"status" : "Success", "task" : %s }' % (task_json))
+    
         
 def serialize_model(model):
     result = {}
