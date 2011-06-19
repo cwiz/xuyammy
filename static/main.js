c44e68f30eb8a23450cfff3c06502284cce1bbc4
Marat Dyatko <marat@ostrovok.ru>
2011-06-19 14:03:15 +0000

    - Generate stories

--- static/main.js
+++ static/main.js
@@ -1,13 +1,49 @@
 $(document).ready(function(){
 	
-	$('#desk tbody td').sortable({
-		connectWith: '#desk tbody td',
-		items: 'div.task',
-		placeholder: 'task placeholder'
-	});
-	$('div.task').task();
+	data.get();
 
+	$('div.task').task();
 });
+
+var data = {
+	get: function(){
+		$.getJSON('/data/', data.parse);
+	},
+	parse: function(resp){
+		var order = ['tags', 'stories', 'tasks'];
+		for(var i = 0; i < order.length; i++)
+		{
+			var id = order[i];
+			if((id in resp) && ('parse_' + id in data)) data['parse_' + id](resp[id]);
+		}
+	},
+	parse_stories: function(data){
+		for(var id in data)
+		{
+			var item = data[id],
+			    story = $('thead[story=' + id + '], tbody[story=' + id + ']');
+			
+			if(!story.length)
+			{
+				story = $.tmpl('<thead story="${id}"><tr><td colspan="3"><h3></h3></td></tr></thead>\
+				<tbody story="${id}"><tr>\
+					<td><h6><strong>open</strong></h6></td>\
+					<td class="progress"><h6><strong>in progress</strong></h6></td>\
+					<td class="ready"><h6><strong>done</strong></h6></td>\
+				</tr></tbody>', item).appendTo('#desk table');
+				
+				$('#desk tbody td').sortable({
+					connectWith: story.find('tbody td'),
+					items: 'div.task',
+					placeholder: 'task placeholder'
+				});
+			}
+
+			story.find('h3').text(item.title);
+		}
+	}
+};
+
 
 $.fn.task = function(){
 	return $(this).each(function(){
