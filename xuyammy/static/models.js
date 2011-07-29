e4cd563894046adc6f3eb71681a0a9b23dc8eeac
Marat Dyatko <marat@ostrovok.ru>
2011-07-29 19:10:08 +0000

    - Start working with backbone.js

---
+++ xuyammy/static/models.js
@@ -0,0 +1,43 @@
+// User
+var UserModel = Backbone.Model.extend({
+	defaults: {
+	}
+});
+
+// Project
+var ProjectModel = Backbone.Model.extend({
+	defaults: {
+	}
+});
+
+// Sprint
+var SprintModel = Backbone.Model.extend({
+	defaults: {
+		text: ''
+	}
+});
+
+// Story
+var StoryModel = Backbone.Model.extend({
+	defaults: {
+		text: ''
+	}
+});
+
+// The Task
+var TaskModel = Backbone.Model.extend({
+	defaults: {
+		text: ''
+	}
+});
+
+// Comment
+var CommentModel = Backbone.Model.extend({
+	defaults: {
+	}
+});
+
+
+// SprintCollection
+// TaskCollection
+// CommentCollection
---
+++ xuyammy/static/views.js
@@ -0,0 +1,17 @@
+// Main view contains header, filters panel, main desk
+var Xuyammy = Backbone.View.extend({
+	el: 'body',
+	initialize: function(){
+		
+	}
+});
+
+// Desk view contains stories table of the sprint
+var Desk = Backbone.View.extend({
+	tag: 'div',
+	id: 'desk',
+	initialize: function(){
+		
+	}
+});
+
